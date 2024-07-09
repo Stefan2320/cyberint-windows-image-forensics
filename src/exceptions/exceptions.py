30f89fbc64b1838ab1690fc1e75d09611134c308
Stefan2320 <stefan.paraschiv023@gmail.com>
2024-07-09 09:02:09 +0300

    feat: add error type for missing api key

--- src/exceptions/exceptions.py
+++ src/exceptions/exceptions.py
@@ -1,12 +1,19 @@
-
-
 class CustomError(Exception):
     """ CustomError to be aware it's from the implementation """
     pass
+
 
 class DriveError(CustomError):
     """ Raised when there is an issue reading the drives from the mounted image """
     def __init__(self, message="An error occured when reading the drives from the mounted image."):
         self.message = message
         super().__init__(self.message)
+
+
+class APIKeyVTError(CustomError):
+    """Raised when the api key for Vt is missing """
+    def __init__(self, message="There is no API key for VirusTotal, please make sure the api key exists."):
+        self.message = message
+        super().__init__(self.message)
+
     
