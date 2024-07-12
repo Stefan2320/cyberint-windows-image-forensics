851f02c3e4610bfd03bf9ebf9dafca1789b0338a
Stefan2320 <stefan.paraschiv023@gmail.com>
2024-07-12 03:03:23 +0300

    feat: add the sha1 hasher

--- src/services/Hashing.py
+++ src/services/Hashing.py
@@ -6,3 +6,14 @@ class MD5Hasher(Hasher):
     def compute_hash(self, path: str) -> str:
         with open(path,"rb") as f:
             return hashlib.md5(f.read()).hexdigest()
+    
+    def get_hash_type(self) -> str:
+        return "MD5"
+
+class SHA1Hasher(Hasher):
+    def compute_hash(self, path: str) -> str:
+        with open(path,"rb") as f:
+            return hashlib.sha1(f.read()).hexdigest()
+    
+    def get_hash_type(self) -> str:
+        return "SHA1"
