4c6a35dfe8d8a716c50c70214c8cdaaa8394af6c
Stefan2320 <stefan.paraschiv023@gmail.com>
2024-07-16 02:35:53 +0300

    feat: add a generic extractor

--- src/models/Info_extractor.py
+++ src/models/Info_extractor.py
@@ -3,34 +3,46 @@ from Registry import Registry
 
 class Extractor(ABC):
 
-    def __init__(self, keyword: str = None):
-        self.keyword = keyword
+    def __init__(self, reg_key: str = None):
+        self.reg_key = reg_key
         
     @abstractmethod
-    def extract(self, location: str):
+    def extract(self, location: str = None):
         pass
 
-    def rec(self, key, keyword: str,paths=None,depth=0):
+    def rec(self, key, reg_key: str,paths=None,depth=0):
         if paths is None:
             paths = []
-        if keyword in key.path():
+        if reg_key in key.path():
             paths.append(key.path())
         for subkey in key.subkeys():
-            self.rec(subkey,keyword, paths,depth + 1)
+            self.rec(subkey,reg_key, paths,depth + 1)
         return paths
+
+class GeneralExtractor(Extractor):
+    def __init__(self, location: str = None, reg_hive: str = None, reg_key: str = None):
+        super().__init__()
+        self.location = location
+        self.reg_hive = reg_hive
+        self.reg_key = reg_key
+        
+    def extract(self):
+        self.data = []
+        reg = Registry.Registry(self.location+self.reg_hive)
+        keys = self.rec(reg.root(),self.reg_key)
+        for subkeys in keys: 
+            key = reg.open(subkeys[5:])
+            for value in [v for v in key.values() \
+                            if v.value_type() == Registry.RegSZ or \
+                                v.value_type() == Registry.RegExpandSZ]:
+                self.data.append({value.name():[value.value(), str(key)[:-28]]})
+        return self.data
 
 class UserExtractor(Extractor):
     def extract(self, location: str):
         self.users = []
         reg = Registry.Registry(f"{location}/Windows/System32/config/SOFTWARE")
-        keys = self.rec(reg.root(),self.keyword)
-        # TODO
-        # regex_pattern = r"S-[a-zA-Z0-9\-]+"
-        # p = re.compile(regex_pattern)
-        # for element in list(keys):
-        #     print(p.match(element))
-        #     if p.match(element):
-        #         print(element)
+        keys = self.rec(reg.root(),self.reg_key)
         for subkeys in keys: 
             key = reg.open(subkeys[5:])
             for value in [v for v in key.values() \
@@ -48,7 +60,7 @@ class HostExtractor(Extractor):
         '''
         self.hosts = []
         reg = Registry.Registry(f"{location}/Windows/System32/config/SYSTEM")
-        keys = self.rec(reg.root(),self.keyword)
+        keys = self.rec(reg.root(),self.reg_key)
         for subkeys in keys: 
             key = reg.open(subkeys[5:])
             for value in [v for v in key.values() \
@@ -63,7 +75,7 @@ class IPExtractor(Extractor):
         '''
         self.ip = []
         reg = Registry.Registry(f"{location}/Windows/System32/config/SYSTEM")
-        keys = self.rec(reg.root(),self.keyword)
+        keys = self.rec(reg.root(),self.reg_key)
         for subkeys in keys: 
             key = reg.open(subkeys[5:])
             for value in [v for v in key.values() \
@@ -79,7 +91,7 @@ class GUIDExtractor(Extractor):
         reg = Registry.Registry(f"{location}/Windows/System32/config/SOFTWARE")
         key = reg.open("Microsoft\\Cryptography")
         for value in [v for v in key.values() if v.value_type() in [Registry.RegSZ, Registry.RegExpandSZ]]:
-            if value.name() == self.keyword:
+            if value.name() == self.reg_key:
                 return {value.name(): [value.value(),str(key)[:-28]]}
                 break
         return None 
