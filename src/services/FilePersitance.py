5b7001e06fad8acbf78217f310f719f3eb10a119
Stefan2320 <stefan.paraschiv023@gmail.com>
2024-07-19 22:04:50 +0300

    fix: how the persistance class takes the path

--- src/services/FilePersitance.py
+++ src/services/FilePersitance.py
@@ -4,17 +4,22 @@ import os
 
 class FilePersistance:
 
-#    user_persistance = "Users\USERNAME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-    system_persistance = "ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+    system_persistance_path = "ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
 
     def __init__(self, root_directory) -> None:
         self.root_directory = root_directory.replace('/','\\') + '\\'
+        self.user_persistance = []
+        self.system_persistance = []
         pass
    
-    def find_system_persistance(self):
-        pass
+    def find_user_persistance(self, usernames: list[str]):
+        for user in usernames:
+            file_path = self.root_directory + f"Users\{user}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"   
+            files = os.listdir(file_path) 
+            self.user_persistance.append(files)
 
     def find_system_persistance(self):
-        file_path = self.root_directory + self.system_persistance
+        file_path = self.root_directory + self.system_persistance_path
         files = os.listdir(file_path) 
+        self.system_persistance = files
     
