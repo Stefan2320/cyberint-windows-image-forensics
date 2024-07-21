88382e0740eacff451cf2c2b00310af82106571d
Stefan2320 <stefan.paraschiv023@gmail.com>
2024-07-21 20:00:51 +0300

    refactor: remove classes that were not useful

--- src/repository/FileRepo.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-from .models import FileModel
-
-class FileRepository:
-    def __init__(self, session: Session):
-        self.session = session
-
-    def get_unknown_files(self) -> bool:
-        return self.session.query(FileModel).filter(FileModel.is_malicious == None).all()
