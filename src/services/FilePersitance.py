9439624497efbac1a2bbdedd3d8586491b00f95c
Stefan2320 <stefan.paraschiv023@gmail.com>
2024-07-21 19:48:00 +0300

    feat: fix how results are added into the user_persistance list

--- src/services/FilePersitance.py
+++ src/services/FilePersitance.py
@@ -12,14 +12,17 @@ class FilePersistance:
         self.system_persistance = []
         pass
    
-    def find_user_persistance(self, usernames: list[str]):
+    def find_user_persistance(self, usernames: list[str]) -> list:
         for user in usernames:
             file_path = self.root_directory + f"Users\{user}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"   
             files = os.listdir(file_path) 
-            self.user_persistance.append(files)
+            for file in files:
+                self.user_persistance.append(file)
+        return self.user_persistance
 
-    def find_system_persistance(self):
+    def find_system_persistance(self) -> list:
         file_path = self.root_directory + self.system_persistance_path
         files = os.listdir(file_path) 
         self.system_persistance = files
+        return self.system_persistance
     
